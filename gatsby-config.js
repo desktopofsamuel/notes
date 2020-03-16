107b3e32d0719e488f0198fe5ffab647ba0ed767
Samuel W <sammatwong@gmail.com>
2020-03-16 16:05:01 +0800

    Attempt to Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,7 +127,6 @@ module.exports = {
                         template
                         draft
                         description
-                        socialImage
                         url
                       }
                     }
